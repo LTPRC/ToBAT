0606a1f466941c23bb4cd3f2f3836ee96cffc3a8
Tuo Li <lit8@miamioh.edu>
2021-01-18 14:56:25 +0800

    Concurrent models

---
+++ src/main/java/com/github/ltprc/juc/model/PipedModel.java
@@ -0,0 +1,70 @@
+package com.github.ltprc.juc.model;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+/**
+ * 使用管道实现生产者消费者模型
+ * @author ZGJ
+ * @date 2017年6月30日
+ */
+public class PipedModel {
+    final PipedInputStream pis = new PipedInputStream();
+    final PipedOutputStream pos = new PipedOutputStream();
+    {
+        try {
+            pis.connect(pos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        PipedModel pipedModel = new PipedModel();
+        new Thread(pipedModel.new Producer()).start();
+        new Thread(pipedModel.new Consumer()).start();
+    }
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while(true) {
+                    Thread.sleep(1000);
+                    int num = (int) (Math.random() * 255);
+                    System.out.println(Thread.currentThread().getName() + "生产者生产了一个数字，该数字为： " + num);
+                    pos.write(num);
+                    pos.flush();
+                } 
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    pos.close();
+                    pis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while(true) {
+                    Thread.sleep(1000);
+                    int num = pis.read();
+                    System.out.println("消费者消费了一个数字，该数字为：" + num);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    pos.close();
+                    pis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
