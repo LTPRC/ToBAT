cecf72f0b1de39317cb6e5883a45c2a83a736a3e
Tuo Li <lit8@miamioh.edu>
2021-02-07 23:37:40 +0800

    fix singleton sample

--- src/main/java/com/github/ltprc/designpattern/creational/singleton/SingletonEnum.java
+++ src/main/java/com/github/ltprc/designpattern/creational/singleton/SingletonEnum.java
@@ -3,7 +3,7 @@ package com.github.ltprc.designpattern.creational.singleton;
 public enum SingletonEnum {
     INSTANCE;
     private static SingletonInstance SINGLETON_INSTANCE = new SingletonInstance();
-    public SingletonInstance getInstance() {
+    public static SingletonInstance getInstance() {
         return SINGLETON_INSTANCE;
     }
 }
